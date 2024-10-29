c9e67bfb5c12fcf59a759845bb698c2057cdd907
kushal-10 <kushalkoshti11@gmail.com>
2024-10-29 06:21:43 +0100

    update splitting

--- retrieval/similarity_search.py
+++ retrieval/similarity_search.py
@@ -1,11 +1,11 @@
 from langchain.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import TextLoader
 
 def generate_docs(loader):
     documents = loader.load()
-    text_splitter = CharacterTextSplitter(chunk_size=200, chunk_overlap=20)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
     texts = text_splitter.split_documents(documents)
     
     # Use open-source embeddings from Hugging Face's sentence-transformers
@@ -22,18 +22,6 @@ def generate_docs(loader):
 if __name__ == "__main__":
     loader = TextLoader("annual_txts/USA/3.NVIDIA_$2.638 T_Information Tech/2023/results.txt")
     docs = generate_docs(loader)
-    loader2 = TextLoader("annual_txts/USA/3.NVIDIA_$2.638 T_Information Tech/2023/results.txt")
-    docs2 = generate_docs(loader2)
-
-    print("FETCHED DOCS for nvidia #############################################################################")
-    for doc in docs:
-        print(doc.page_content)
-        print("-"*100)
-   
-    print("FETCHED DOCS for google #############################################################################")
-    for doc in docs2:
-        print(doc.page_content)
-        print("-"*100)
-    
     print(len(docs))
-    print(len(docs2))
+    print(docs)
+  
