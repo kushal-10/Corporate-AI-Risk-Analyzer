1d599fc1b47c312b30ce4846a0b136a4a1b6319f
kushal-10 <kushalkoshti11@gmail.com>
2024-10-29 06:05:05 +0100

    update prompt

--- retrieval/similarity_search.py
+++ retrieval/similarity_search.py
@@ -13,22 +13,7 @@ def generate_docs(loader):
     db = FAISS.from_documents(texts, embeddings)
 
     search_query = """
-    Find text mentioning or discussing advancements, risks, or opportunities in:
-    - Artificial Intelligence (AI)
-    - Machine Learning (ML)
-    - Cybersecurity
-    - Data Science
-    - Automation
-    - Neural Networks
-    - Robotics
-    - Big Data
-    - Deep Learning
-    - Innovation in technology
-
-    Include sections discussing technology-related risks, benefits, regulatory impacts, and strategic initiatives.
-    Focus on phrases like "AI strategy," "AI risks," "impact of AI," "AI-driven," "ML-based," "cyber threats," "security risks," "automation benefits," and "technological innovation."
-
-    Retrieve relevant content that may discuss any emerging technologies, ethical considerations, regulatory concerns, or competitive advantages associated with these technologies.
+    advancements, risks, or opportunities in AI, ML, cybersecurity, data science, automation, neural networks, robotics, big data, deep learning, or technology innovation.
     """
 
     retriever = db.as_retriever(
@@ -41,9 +26,18 @@ def generate_docs(loader):
 if __name__ == "__main__":
     loader = TextLoader("annual_txts/USA/3.NVIDIA_$2.638 T_Information Tech/2023/results.txt")
     docs = generate_docs(loader)
-    print("FETCHED DOCS #############################################################################")
+    loader2 = TextLoader("annual_txts/USA/3.NVIDIA_$2.638 T_Information Tech/2023/results.txt")
+    docs2 = generate_docs(loader2)
+
+    print("FETCHED DOCS for nvidia #############################################################################")
     for doc in docs:
+        print(doc.page_content)
+        print("-"*100)
+   
+    print("FETCHED DOCS for google #############################################################################")
+    for doc in docs2:
         print(doc.page_content)
         print("-"*100)
     
     print(len(docs))
+    print(len(docs2))
