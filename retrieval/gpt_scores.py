0a57c337891c1e82c00cce67c0a6447c4964cabd
kushal-10 <kushalkoshti11@gmail.com>
2024-12-01 22:08:24 +0100

    add gpt labels

--- retrieval/gpt_scores.py
+++ retrieval/gpt_scores.py
@@ -1,4 +1,8 @@
 from openai import OpenAI
+import os
+import json
+from tqdm import tqdm
+
 client = OpenAI()
 
 BASE_PROMPT = """
@@ -18,7 +22,39 @@ def classify(input_prompt: str):
 
     return completion.choices[0].message.content
 
+def get_labels(json_path: str, output_path: str):
+    
+    with open(json_path, 'r') as f:
+        json_data = json.load(f)
+
+    if not os.path.exists(output_path):
+        label_metadata = {}
+    else:
+        with open(output_path, 'r') as f:
+            label_metadata = json.load(f)
+
+    keys = list(json_data.keys())
+    processed_keys = list(label_metadata.keys())
+
+    for k in tqdm(keys):
+        if k not in processed_keys:
+            passages = json_data[k]
+            labels = []
+            for p in passages:
+                text_content = p[1]
+                label = classify(text_content)
+                labels.append([label, text_content])
+            
+            label_metadata[k] = labels
+            with open(output_path, 'w') as f:
+                json.dump(label_metadata, f, indent=4)
+
+
 if __name__=='__main__':
-    input_prompt = "Artificial Intelligence"
-    label = classify(input_prompt) 
-    print(label, type(label)) # NEGATIVE, STR
+
+    json_path = os.path.join('retrieval', 'retrieved_docs.json')
+    output_path = os.path.join('retrieval', 'gpt_labels.json')
+    
+    get_labels(json_path, output_path)
+
+
